0f257bcbdee1e8f84c818b96166a344c1a10f210
alex <o4kan@ya.ru>
2014-09-25 12:41:19 +0400

    add unregister push ios

--- mount.sh
+++ mount.sh
@@ -3,4 +3,4 @@ sudo rm -rf /var/www/phonegap/lib/iphone4/
 sudo mkdir /var/www/phonegap/lib/iphone4/
 sudo chown alex:alex /var/www/phonegap/lib/iphone4/
 ifuse /var/www/phonegap/lib/iphone4/ --root
-mv ~/Загрузки/AppleTaxiDrivers.ipa /var/www/phonegap/lib/iphone4/var/mobile/Downloads/AppleTaxiDrivers.ipa
+mv ~/Загрузки/ATDrivers.ipa /var/www/phonegap/lib/iphone4/var/mobile/Downloads/ATDrivers.ipa
